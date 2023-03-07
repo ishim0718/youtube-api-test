42a88fe6549ff0aa5e71009451b1c4c249f42927
ishim0718 <ishim0718@gmail.com>
2023-03-06 21:28:00 -0500

    data api working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
-fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=viewCount&safeSearch=moderate&type=video&videoEmbeddable=true&key=[AIzaSyCabDHkqaj_hlSQTQo4JrNKMgByCyNTQ0k]")
+//const youtubeApi = AIzaSyAWHwk1CEaivU5yaXOeni7b65XTn6GCYWk
+var searchTopic = document.querySelector(".search");
+
+//fetch 5 results for q=excercize; todo: make search result be q
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=none&maxResults=5&order=viewCount&q=excercize&type=video&videoEmbeddable=true&key=AIzaSyAWHwk1CEaivU5yaXOeni7b65XTn6GCYWk")
 .then(data => data.json())
 .then(list => console.log(list)); 
